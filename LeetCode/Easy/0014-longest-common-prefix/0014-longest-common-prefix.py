860e35059d839181093108da2d5e5a3d0924990b
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-01-11 23:39:42 +0900

    Time: 39 ms (74.02%), Space: 13.9 MB (47.59%) - LeetHub

---
+++ LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        shortest = ""
+        shortest_len = float("inf")
+        result = ""
+
+        for i, v in enumerate(strs):
+            if len(v) < shortest_len:
+                shortest = v
+                shortest_len = len(v)
+        
+        for i in range(shortest_len):
+            temp = shortest[i]
+            condition = True
+            for v in strs:
+                if shortest == v:
+                    continue
+                if v[i] != temp:
+                    condition = False
+                    break
+            if condition:
+                result += temp
+            else:
+                break
+        return result
+            
+        
+        
+        
