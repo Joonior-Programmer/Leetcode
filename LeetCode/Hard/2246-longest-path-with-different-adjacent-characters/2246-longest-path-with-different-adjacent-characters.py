5a60998d324d8341cb0226c9f57b7567317fde01
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-01-13 23:57:18 +0900

    Time: 1659 ms (91.70%), Space: 143.4 MB (87.25%) - LeetHub

--- LeetCode/Hard/2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ LeetCode/Hard/2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -5,28 +5,6 @@ class Solution:
         
         for i in range(1, len(parent)):
             tree[parent[i]].append(i)
-
-        # def DFS(current, count):
-        #     temp_arr = []
-        #     temp_arr_len = 0
-        #     for v in tree[current]:
-        #         if s[v] == s[current]:
-        #             temp = DFS(v, 1)
-        #             if temp + 1 > self.result:
-        #                 self.result = temp + 1
-        #         else:
-        #             temp_arr.append(DFS(v, count + 1) - 1)
-        #             temp_arr_len += 1
-        #     temp_arr.sort(reverse=True)
-        #     result = 0
-        #     print(current, temp_arr, count)
-        #     if len(temp_arr) > 1:
-        #         count = temp_arr[0] + temp_arr[1]
-        #     elif len(temp_arr) == 1:
-        #         count = temp_arr[0]
-        #     # count = temp_arr[0] + temp_arr[1]
-        #     print(current, temp_arr,count)
-        #     return count
         
         def DFS(current):
             max1 = 0
@@ -39,8 +17,8 @@ class Solution:
                     max1 = temp
                 elif temp > max2:
                     max2 = temp    
-            if self.result < max1 + max2 + 1:
-                self.result = max1 + max2 + 1
+            
+            self.result = max(self.result, max1 + max2 + 1)
             return max1 + 1
 
         DFS(0)
