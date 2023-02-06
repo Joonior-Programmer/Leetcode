c44f265f334c307410d6a635ba74fdc7f7ae2e1c
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-02-06 22:31:45 +0900

    Delete 1470-shuffle-the-array.py

--- LeetCode/Easy/1470-shuffle-the-array/1470-shuffle-the-array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def shuffle(self, nums: List[int], n: int) -> List[int]:
-        front = nums[:n]
-        back = nums[n:]
-        ret = []
-        for i in range(n):
-            ret.append(front[i])
-            ret.append(back[i])
-        return ret
