6762bac88a7ee494edc42aba2d1776406520cece
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-02-06 23:27:34 +0900

    Time: 8 ms (5.72%), Space: 6.9 MB (12.30%) - LeetHub

---
+++ LeetCode/Easy/0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ret;
+        for (int i = 0; i < numRows; ++i){
+            vector<int> temp;
+            for(int j = 0; j < i+1; ++j){
+                if (j == i || j == 0){
+                    temp.push_back(1);
+                } else {
+                    cout << ret[i-1][j-1] << " " << ret[i-1][j] << endl;
+                    temp.push_back(ret[i-1][j-1] + ret[i-1][j]);
+                }
+            }
+            ret.push_back(temp);
+        }
+        return ret;
+    }
+};
+
+// class Solution {
+//     public List<List<Integer>> generate(int numRows) {
+//         List<List<Integer>> ret = new ArrayList<>();
+
+//         for (int i = 0; i < numRows; ++i){
+//             List<Integer> temp = new ArrayList<>();
+//             for(int j = 0; j < i+1; ++j){
+//                 if (i == j || j == 0){
+//                     temp.add(1);
+//                 } else {
+//                     temp.add(ret.get(i-1).get(j-1) + ret.get(i-1).get(j));
+//                 }
+//             }
+//             ret.add(temp);
+//         }
+//         return ret;
+//     }
+// }
