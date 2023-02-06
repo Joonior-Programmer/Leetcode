1aff47aca481e0f0ba7c350464b81a757a8f7dda
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-02-06 22:55:47 +0900

    Time: 7 ms (62.32%), Space: 9.5 MB (99.83%) - LeetHub

--- LeetCode/Easy/1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ LeetCode/Easy/1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> front(nums.begin(), nums.begin()+n);
-        vector<int> back(nums.begin()+n, nums.end());
         vector<int> ret(n*2);
         for(int i = 0; i < n; ++i){
-            ret[i*2] = front[i];
-            ret[i*2+1] = back[i];
+            ret[i*2] = nums[i];
+            ret[i*2+1] = nums[i+n];
         }
         return ret;
     }
