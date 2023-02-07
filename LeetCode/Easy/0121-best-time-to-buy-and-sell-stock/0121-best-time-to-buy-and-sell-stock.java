1c0cb846bd5d5d25d0f72cf34f0327e232c77e46
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-02-07 23:50:19 +0900

    Time: 2 ms (67.64%), Space: 58.7 MB (85.87%) - LeetHub

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -3,10 +3,11 @@ class Solution {
         int profit = 0;
         int minVal = prices[0];
         for (int i = 1; i < prices.length; ++i){
-            if (minVal > prices[i]) {
-                minVal = prices[i];
-                continue;
-            }
+            // if (minVal > prices[i]) {
+            //     minVal = prices[i];
+            //     continue;
+            // }
+            minVal = Math.min(minVal, prices[i]);
             profit = Math.max(profit, prices[i] - minVal);
         }
         return profit;
