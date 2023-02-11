46fc89f7895a75a91474ba83253176c92583c9c2
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-02-11 22:28:10 +0900

    Time: 0 ms (100.00%), Space: 2 MB (24.81%) - LeetHub

---
+++ LeetCode/Easy/0027-remove-element/0027-remove-element.go
@@ -0,0 +1,10 @@
+func removeElement(nums []int, val int) int {
+    k := 0
+    for i := 0; i < len(nums); i++ {
+        if nums[i] != val {
+            nums[k] = nums[i]
+            k++
+        }
+    }
+    return k
+}
