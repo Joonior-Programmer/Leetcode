08da1929fa304c26a2334f67d9cbdca146551b86
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-02-11 22:29:17 +0900

    Time: 41 ms (34.13%), Space: 13.9 MB (53.96%) - LeetHub

--- LeetCode/Easy/0027-remove-element/0027-remove-element.py
+++ LeetCode/Easy/0027-remove-element/0027-remove-element.py
@@ -1,23 +1,29 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        l, r = 0, len(nums) - 1
-        for i in range(r, -1, -1):
-            if nums[i] == val:
-                r -= 1
-            else:
-                break
+#         l, r = 0, len(nums) - 1
+#         for i in range(r, -1, -1):
+#             if nums[i] == val:
+#                 r -= 1
+#             else:
+#                 break
         
-        while l < r:
-            if nums[l] == val:
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
-                r -= 1
-            else:
-                l += 1
+#         while l < r:
+#             if nums[l] == val:
+#                 nums[l], nums[r] = nums[r], nums[l]
+#                 l += 1
+#                 r -= 1
+#             else:
+#                 l += 1
 
-            for i in range(r, -1, -1):
-                if nums[i] == val:
-                    r -= 1
-                else:
-                    break
-        return r + 1
+#             for i in range(r, -1, -1):
+#                 if nums[i] == val:
+#                     r -= 1
+#                 else:
+#                     break
+#         return r + 1
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
