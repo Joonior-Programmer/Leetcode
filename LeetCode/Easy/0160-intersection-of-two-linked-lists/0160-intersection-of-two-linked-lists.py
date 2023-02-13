2b1faaaf57526ef5d40774e82c686f2aea3d4406
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-02-13 23:15:49 +0900

    Time: 176 ms (26.89%), Space: 29.5 MB (66.48%) - LeetHub

--- LeetCode/Easy/0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ LeetCode/Easy/0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,14 +6,43 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        exist = set()
-        while headA:
-            exist.add(headA)
-            headA = headA.next
+#         exist = set()
+#         while headA:
+#             exist.add(headA)
+#             headA = headA.next
         
-        while headB:
-            if headB in exist:
-                return headB
+#         while headB:
+#             if headB in exist:
+#                 return headB
+#             headB = headB.next
+        
+#         return None
+        len_A = 0
+        len_B = 0
+        cur_A = headA
+        cur_B = headB
+        while cur_A:
+            len_A += 1
+            cur_A = cur_A.next
+            
+        while cur_B:
+            len_B += 1
+            cur_B = cur_B.next
+            
+        if len_A > len_B:
+            while len_A > len_B:
+                headA = headA.next
+                len_B += 1
+        elif len_B > len_A:
+            while len_B > len_A:
+                headB = headB.next
+                len_A += 1
+        
+        while headA and headB:
+            if headA == headB:
+                return headA
+            headA = headA.next
             headB = headB.next
         
         return None
+                
