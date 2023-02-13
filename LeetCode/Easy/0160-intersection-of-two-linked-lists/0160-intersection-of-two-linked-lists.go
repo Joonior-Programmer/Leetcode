59873354c3ed12bcdf9989b1eb6eeda33e5413f2
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-02-13 23:01:49 +0900

    Time: 28 ms (91.82%), Space: 7.2 MB (53.69%) - LeetHub

--- LeetCode/Easy/0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.go
+++ LeetCode/Easy/0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.go
@@ -6,19 +6,56 @@
  * }
  */
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
-    exist := make(map[*ListNode]bool)
+    // exist := make(map[*ListNode]bool)
     
-    for headA != nil {
-        exist[headA] = true
-        headA = headA.Next
+    // for headA != nil {
+    //     exist[headA] = true
+    //     headA = headA.Next
+    // }
+    
+    // for headB != nil {
+    //     if exist[headB] {
+    //         return headB
+    //     }
+    //     headB = headB.Next
+    // }
+    
+    // return nil
+
+    lenA := 0
+    lenB := 0
+    curA := headA
+    curB := headB
+
+    for curA != nil {
+        lenA++
+        curA = curA.Next
     }
-    
-    for headB != nil {
-        if exist[headB] {
-            return headB
+
+    for curB != nil {
+        lenB++
+        curB = curB.Next
+    }
+
+    if lenA > lenB {
+        for lenA > lenB {
+            headA = headA.Next
+            lenB++
         }
+    } else if lenB > lenA {
+        for lenB > lenA {
+            headB = headB.Next
+            lenA++
+        }
+    }
+
+    for headA != nil && headB != nil {
+        if headA == headB {
+            return headA
+        }
+        headA = headA.Next
         headB = headB.Next
     }
-    
+
     return nil
 }
