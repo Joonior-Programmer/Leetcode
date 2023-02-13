ee82b29afab45361478e8d146c6ef70c7ba96be6
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-02-13 23:12:19 +0900

    Time: 41 ms (95.35%), Space: 14.4 MB (90.35%) - LeetHub

--- LeetCode/Easy/0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ LeetCode/Easy/0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,17 +9,53 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        set<ListNode*> exist;
+//         set<ListNode*> exist;
         
-        while (headA) {
-            exist.insert(headA);
-            headA = headA->next;
+//         while (headA) {
+//             exist.insert(headA);
+//             headA = headA->next;
+//         }
+        
+//         while (headB) {
+//             if (exist.find(headB) != exist.end()){
+//                 return headB;
+//             }
+//             headB = headB->next;
+//         }
+        
+//         return NULL;
+        int lenA = 0;
+        int lenB = 0;
+        ListNode* curA = headA;
+        ListNode* curB = headB;
+        
+        while (curA){
+            ++lenA;
+            curA = curA->next;
         }
         
-        while (headB) {
-            if (exist.find(headB) != exist.end()){
-                return headB;
+        while (curB){
+            ++lenB;
+            curB = curB->next;
+        }
+        
+        if(lenA > lenB){
+            while (lenA > lenB){
+                headA = headA->next;
+                lenB++;
             }
+        } else if (lenB > lenA){
+            while (lenB > lenA){
+                headB = headB->next;
+                lenA++;
+            }
+        }
+        
+        while (headA && headB) {
+            if (headA == headB){
+                return headA;
+            }
+            headA = headA->next;
             headB = headB->next;
         }
         
