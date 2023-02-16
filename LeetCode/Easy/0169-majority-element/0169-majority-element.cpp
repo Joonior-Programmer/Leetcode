8c784b83cce53f11fd56a215de1be9c0b44ecb7e
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-02-16 23:03:41 +0900

    Time: 16 ms (89.48%), Space: 19.6 MB (32.77%) - LeetHub

--- LeetCode/Easy/0169-majority-element/0169-majority-element.cpp
+++ LeetCode/Easy/0169-majority-element/0169-majority-element.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size() / 2];
+        int count = 0;
+        int ret = 0;
+        for ( int i = 0; i < nums.size(); ++i){
+            if (!count){
+                ret = nums[i];
+            }
+            
+            if (ret == nums[i]){
+                ++count;
+            } else {
+                count--;
+            }
+        }
+        return ret;
     }
 };
