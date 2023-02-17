ae3c23af50000a6f96791b1be3fe09c24c21b24f
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-02-18 00:15:35 +0900

    Delete 0171-excel-sheet-column-number.py

--- LeetCode/Easy/0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def titleToNumber(self, columnTitle: str) -> int:
-        ret = 0
-        power = len(columnTitle) - 1
-        for i in range(len(columnTitle)):
-            ret += (ord(columnTitle[i]) - 64) * 26 ** power
-            power -= 1
-        return ret
