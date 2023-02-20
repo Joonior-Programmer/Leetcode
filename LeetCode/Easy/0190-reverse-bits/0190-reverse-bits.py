f05727bd54dfb825bd9add1a84c6d78bc1a90aac
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-02-20 22:58:21 +0900

    Time: 32 ms (78.91%), Space: 13.9 MB (5.11%) - LeetHub

--- LeetCode/Easy/0190-reverse-bits/0190-reverse-bits.py
+++ LeetCode/Easy/0190-reverse-bits/0190-reverse-bits.py
@@ -2,16 +2,16 @@ class Solution:
     def reverseBits(self, n: int) -> int:
             # 0001 = 1
             # 1000 = 8
-        v = ""
-        for i in range(31, -1, -1):
-            temp = 2 ** i
-            if n >= temp:
-                v = "1" + v
-                n -= temp
-            else:
-                v = "0" + v
+#         v = ""
+#         for i in range(31, -1, -1):
+#             temp = 2 ** i
+#             if n >= temp:
+#                 v = "1" + v
+#                 n -= temp
+#             else:
+#                 v = "0" + v
 
-        return int(v, 2)
+#         return int(v, 2)
         # v = bin(n)[2:]
 
         # for i in range(32-len(v)):
@@ -19,3 +19,9 @@ class Solution:
         
         
         # return int("".join(reversed(list(v))), 2)
+        ret = 0
+        for i in range(32):
+            ret <<= 1
+            ret |= n & 1
+            n >>= 1
+        return ret
