a954111fd888baa580628f9ddbd0571ddcebc9fc
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-02-27 22:36:21 +0900

    Delete 0219-contains-duplicate-ii.py

--- LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        find = defaultdict(list)
-        for i in range(len(nums)):
-            if find[nums[i]] and i - find[nums[i]][-1] <= k:
-                return True
-            find[nums[i]].append(i)
-        return False
