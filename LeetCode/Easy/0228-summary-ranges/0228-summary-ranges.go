515b8d4c98f1af7b1a4fdf0f59d061a52f5036ad
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-03-01 23:43:51 +0900

    Time: 0 ms (100.00%), Space: 2 MB (23.60%) - LeetHub

---
+++ LeetCode/Easy/0228-summary-ranges/0228-summary-ranges.go
@@ -0,0 +1,22 @@
+func summaryRanges(nums []int) []string {
+    i := 0
+    length := len(nums)
+    var ret []string
+    for i < length {
+        low := i
+        
+        for i + 1 < length && nums[i+1] == nums[i]+1 {i++}
+        
+        var buf bytes.Buffer
+        buf.WriteString(strconv.Itoa(nums[low]))
+        
+        if low != i {
+            buf.WriteString("->")
+            buf.WriteString(strconv.Itoa(nums[i]))
+        }
+        
+        ret = append(ret, buf.String())
+        i++
+    }
+    return ret
+}
