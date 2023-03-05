5a5bca1cf980742fe6e11413df19764b1a09a32f
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-03-05 22:27:31 +0900

    Time: 5 ms (48.36%), Space: 42.3 MB (74.36%) - LeetHub

---
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] a = new int[26];
+        int[] b = new int[26];
+        for (int i = 0; i < s.length(); ++i){
+            ++a[s.charAt(i) - 'a'];
+            ++b[t.charAt(i) - 'a'];
+        }
+        for(int i = 0; i < 26; ++i){
+            if (a[i] != b[i]) return false;
+        }
+        
+        return true;
+        
+    }
+}
