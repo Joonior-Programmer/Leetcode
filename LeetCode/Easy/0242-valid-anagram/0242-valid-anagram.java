419cf3e9f80c90a2f8079cb5f43431bde5bc1682
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-03-05 22:40:52 +0900

    Time: 2 ms (95.72%), Space: 42.3 MB (74.36%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
@@ -3,9 +3,11 @@ class Solution {
         if (s.length() != t.length()) return false;
         int[] a = new int[26];
         int[] b = new int[26];
-        for (int i = 0; i < s.length(); ++i){
-            ++a[s.charAt(i) - 'a'];
-            ++b[t.charAt(i) - 'a'];
+        char[] charS = s.toCharArray();
+        char[] charT = t.toCharArray();
+        for (int i = 0; i < charS.length; ++i){
+            ++a[charS[i] - 'a'];
+            ++b[charT[i] - 'a'];
         }
         for(int i = 0; i < 26; ++i){
             if (a[i] != b[i]) return false;
