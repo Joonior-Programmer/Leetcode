df9793b1b23943bfce3bd71c0bab48ad72a1cb9a
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-03-07 22:26:07 +0900

    Time: 3 ms (56.13%), Space: 6 MB (9.75%) - LeetHub

---
+++ LeetCode/Easy/0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int temp = 0;
+        while (num > 9){
+            while (num != 0){
+                temp += num % 10;
+                num /= 10;
+            }
+            num = temp;
+        temp = 0;
+        }
+        return num;
+    }
+};
