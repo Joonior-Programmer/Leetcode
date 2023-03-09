98c074a470533689cac59647e8dec47e0f38a516
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-03-10 01:47:27 +0900

    Time: 15 ms (80.82%), Space: 6.6 MB (12.35%) - LeetHub

--- LeetCode/Easy/0268-missing-number/0268-missing-number.go
+++ LeetCode/Easy/0268-missing-number/0268-missing-number.go
@@ -1,7 +1,6 @@
 func missingNumber(nums []int) int {
-    sum := 0
-    originalSum := 0
-    for i := 0; i < len(nums); i++ {sum += nums[i]}
-    for i := 1; i < len(nums)+1; i++ {originalSum += i}
-    return originalSum - sum
+    n := len(nums)
+    for i, num := range nums { n ^= i ^ num}
+    return n
+
 }
