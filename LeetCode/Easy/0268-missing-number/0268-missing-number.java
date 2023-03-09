8dbcf7dbe386eca16ff362e3fe41ea4b6629e5c5
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-03-10 01:27:46 +0900

    Time: 0 ms (100.00%), Space: 43.1 MB (61.05%) - LeetHub

--- LeetCode/Easy/0268-missing-number/0268-missing-number.java
+++ LeetCode/Easy/0268-missing-number/0268-missing-number.java
@@ -1,6 +1,7 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int sum = Arrays.stream(nums).sum();
+        int sum = 0;
+        for (int i = 0; i < nums.length; ++i) sum += nums[i];
         int originalSum = 0;
         for (int i = 1; i < nums.length + 1; ++i){
             originalSum += i;
