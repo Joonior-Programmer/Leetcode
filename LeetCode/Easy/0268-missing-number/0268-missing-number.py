07c00ff9c5c96d5b728cf3e114faeeaf70356899
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-03-10 01:15:58 +0900

    Delete 0268-missing-number.py

--- LeetCode/Easy/0268-missing-number/0268-missing-number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        min_num = -1
-        for num in nums:
-            if num > min_num + 1:
-                return min_num + 1
-            min_num = num
-        return min_num + 1
