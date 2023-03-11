108e6fd9a22806089d45193301f53f497b031be2
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-03-12 00:16:52 +0900

    Time: 154 ms (98.75%), Space: 15.6 MB (52.73%) - LeetHub

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.py
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.py
@@ -5,7 +5,7 @@ class Solution:
         """
         j = 0
         for i in range(len(nums)):
-            if nums[i] == 0:
-                continue
-            nums[i], nums[j] = nums[j], nums[i]
-            j += 1
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+            
