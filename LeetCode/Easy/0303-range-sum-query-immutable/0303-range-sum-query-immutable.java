1e4abf24adcc18c9aa14e833dde1006040de4443
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-03-15 22:34:55 +0900

    Time: 9 ms (59.28%), Space: 45.3 MB (59.54%) - LeetHub

--- LeetCode/Easy/0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ LeetCode/Easy/0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,15 +1,16 @@
 class NumArray {
-    private ArrayList<Integer> sumArray = new ArrayList<>();
+    private int[] sumArray;
     public NumArray(int[] nums) {
-        sumArray.add(nums[0]);
+        sumArray = new int[nums.length];
+        sumArray[0] = nums[0];
         for(int i = 1; i < nums.length; ++i){
-            sumArray.add(sumArray.get(i-1) + nums[i]);
+            sumArray[i] = sumArray[i-1] + nums[i];
         }
     }
     
     public int sumRange(int left, int right) {
-        int ret = sumArray.get(right);
-        if (left != 0) ret -= sumArray.get(left - 1);
+        int ret = sumArray[right];
+        if (left != 0) ret -= sumArray[left - 1];
         return ret;
     }
 }
