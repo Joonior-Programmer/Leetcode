496702120d42416d1021efd1a676514ac6635745
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-03-18 22:27:59 +0900

    Time: 1 ms (100.00%), Space: 39.6 MB (63.52%) - LeetHub

--- LeetCode/Easy/0342-power-of-four/0342-power-of-four.java
+++ LeetCode/Easy/0342-power-of-four/0342-power-of-four.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if (n < 1 || n % 4 != 0) return n == 1;
+        if (n % 4 != 0) return n == 1;
         while (n > 3) {
             if (n % 4 != 0) return false;
             n /= 4;
