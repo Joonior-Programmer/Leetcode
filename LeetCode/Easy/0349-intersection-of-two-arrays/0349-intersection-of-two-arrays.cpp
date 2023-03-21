74a2bc34370471e95d174f432a100617dc81fddc
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-03-21 23:04:29 +0900

    Time: 9 ms (45.07%), Space: 10.7 MB (20.12%) - LeetHub

---
+++ LeetCode/Easy/0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ret;
+        map<int, bool> a;
+
+        for (int i = 0; i < nums1.size(); ++i) a[nums1[i]] = true;
+        
+        for (int i = 0; i < nums2.size(); ++i){
+            if (a[nums2[i]]) {
+                ret.push_back(nums2[i]);
+                a[nums2[i]] = false;
+            }
+        }
+        
+        return ret;
+    }
+};
