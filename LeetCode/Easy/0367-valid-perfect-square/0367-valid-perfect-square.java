1588d60ea8b11e16220ecc29cd6aa671f3ceb300
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-03-23 23:49:06 +0900

    Time: 0 ms (100.00%), Space: 39.3 MB (43.73%) - LeetHub

---
+++ LeetCode/Easy/0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long l = 1;
+        long r = num;
+        long mid;
+        while(l < r){
+            mid = (l + r) / 2;
+            if (mid * mid == num) return true;
+            else if (mid * mid < num) l = mid + 1;
+            else r = mid;
+        }
+        if (num * num == num) return true;
+        return false;
+    }
+}
