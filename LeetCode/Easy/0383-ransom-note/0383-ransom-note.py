2a71162fa74da1924d23e72af1ac093a89edc613
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-03-25 22:31:03 +0900

    Delete 0383-ransom-note.py

--- LeetCode/Easy/0383-ransom-note/0383-ransom-note.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        rn = Counter(ransomNote)
-        mz = Counter(magazine)
-        for k, v in rn.items():
-            if k not in mz or mz[k] < v:
-                return False
-        return True
