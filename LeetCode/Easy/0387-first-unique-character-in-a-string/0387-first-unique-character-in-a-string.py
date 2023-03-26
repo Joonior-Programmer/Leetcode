6feaaef7569f4bbefb67d04d49b9fe0c684973a2
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-03-26 22:36:58 +0900

    Time: 71 ms (92.00%), Space: 14.1 MB (47.12%) - LeetHub

--- LeetCode/Easy/0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ LeetCode/Easy/0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,15 +1,14 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        is_exist = [float("inf")] * 26
-        exist = len(s) + 1
-        ret = 0
+        counter = Counter(s)
+        
+        if 1 not in counter.values():
+            return -1
+        
         for i in range(len(s)):
-            location = ord(s[i]) - 97
-            if is_exist[location] != len(s) + 1 and is_exist[location] == float("inf"): 
-                is_exist[location] = i
-            elif is_exist[location] != float("inf"):
-                is_exist[location] = exist
-        return min(is_exist) if min(is_exist) != exist else -1
+            if counter[s[i]] == 1:
+                return i
+            
         
 
                 
