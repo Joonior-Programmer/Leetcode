c711ba25d2ed7b91325e4cb37b4eceacf1f4f92d
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-03-26 23:41:54 +0900

    Time: 26 ms (74.03%), Space: 10.5 MB (93.57%) - LeetHub

--- LeetCode/Easy/0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ LeetCode/Easy/0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,31 +1,14 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char, int> counter;
-        int ret = -1;
+        int ret[26]{0};
         for (int i = 0; i < s.size(); ++i){
-            if (counter.find(s[i]) == counter.end()){
-                counter.insert({s[i], 1});
-            } else {
-                ++counter[s[i]];
-            }
-        }
-        bool exist = false;
-        for(auto iter=counter.begin();iter!=counter.end();iter++){
-            if (iter->second == 1) {
-                exist = true;
-                break;
-            }
+            ++ret[s[i] - 'a'];
         }
         
-        if (!exist) return ret;
-        
         for (int i = 0; i < s.size(); ++i){
-            if(counter[s[i]] == 1) {
-                ret = i;
-                break;
-            }
+            if(ret[s[i] - 'a'] == 1) return i;
         }
-        return ret;
+        return -1;
     }
 };
