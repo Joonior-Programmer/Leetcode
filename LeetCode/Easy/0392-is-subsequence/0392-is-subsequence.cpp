35be3518016e4ff302f0255dcdfc8369c88e2cfc
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-03-28 22:53:51 +0900

    Time: 0 ms (100.00%), Space: 6.5 MB (28.88%) - LeetHub

---
+++ LeetCode/Easy/0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j = 0;
+        for (int i = 0; i < t.size(); ++i){
+            if (j < s.length() && s[j] == t[i]) ++j;
+        }
+        if (j == s.size()) return true;
+        return false;
+    }
+};
