466ed03cfe17576b3ce89e87af5813db7297784a
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-03-28 23:00:38 +0900

    Time: 0 ms (100.00%), Space: 40.6 MB (41.26%) - LeetHub

--- LeetCode/Easy/0392-is-subsequence/0392-is-subsequence.java
+++ LeetCode/Easy/0392-is-subsequence/0392-is-subsequence.java
@@ -1,9 +1,13 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) return true;
         int j = 0;
         char[] sChar = s.toCharArray();
         for (char v : t.toCharArray()){
-            if (j < s.length() && sChar[j] == v) ++j;
+            if (sChar[j] == v) {
+                ++j;
+                if (j == sChar.length) return true;
+            }
         }
         if (j == s.length()) return true;
         return false;
