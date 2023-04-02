7e6cf6d66ff6dcad910717b2537db8fc617c1f3a
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-02 22:55:54 +0900

    Delete 0414-third-maximum-number.py

--- LeetCode/Easy/0414-third-maximum-number/0414-third-maximum-number.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def thirdMax(self, nums: List[int]) -> int:
-        nums = [*set(nums)]
-        if len(nums) < 3:
-            return max(nums)
-        nums.sort()
-        return nums[-3]
