21ee9592d156bafb908c98e59f07d62073b2ea1b
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-02 23:19:55 +0900

    Time: 0 ms (100.00%), Space: 42.3 MB (54.04%) - LeetHub

--- LeetCode/Easy/0414-third-maximum-number/0414-third-maximum-number.java
+++ LeetCode/Easy/0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,17 +1,34 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
+        // HashSet<Integer> set = new HashSet<>();
+        // for (int i = 0; i < nums.length; ++i){
+        //     set.add(nums[i]);
+        // }
+        // if (set.size() < 3) return Collections.max(set);
+        // int[] ret = new int[set.size()];
+        // int i = 0;
+        // for (int v : set){
+        //     ret[i] = v;
+        //     ++i;
+        // }
+        // Arrays.sort(ret);
+        // return ret[ret.length-3];
+        
+        long first = Long.MIN_VALUE;
+        long second = Long.MIN_VALUE;
+        long third = Long.MIN_VALUE;
         for (int i = 0; i < nums.length; ++i){
-            set.add(nums[i]);
+            if (first < nums[i]){
+                third = second;
+                second = first;
+                first = nums[i];
+            } else if (second < nums[i] && nums[i] != first){
+                third = second;
+                second = nums[i];
+            } else if (third < nums[i] && nums[i] != second && nums[i] != first){
+                third = nums[i];
+            }
         }
-        if (set.size() < 3) return Collections.max(set);
-        int[] ret = new int[set.size()];
-        int i = 0;
-        for (int v : set){
-            ret[i] = v;
-            ++i;
-        }
-        Arrays.sort(ret);
-        return ret[ret.length-3];
+        return third == Long.MIN_VALUE ? (int) first : (int)third;
     }
 }
