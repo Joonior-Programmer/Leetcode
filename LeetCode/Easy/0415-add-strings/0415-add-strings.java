82cef5ec24cc0e4a98d80bb04579b92f082b2d2b
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-03 22:58:07 +0900

    Time: 2 ms (92.14%), Space: 42.2 MB (85.46%) - LeetHub

--- LeetCode/Easy/0415-add-strings/0415-add-strings.java
+++ LeetCode/Easy/0415-add-strings/0415-add-strings.java
@@ -4,14 +4,26 @@ class Solution {
         int carry = 0;
         int i = num1.length() - 1;
         int j = num2.length() - 1;
-        while(i > -1 || j > -1 || carry != 0){
-            if (i > -1) carry += num1.charAt(i) - 48;
-            if (j > -1) carry += num2.charAt(j) - 48;
+        while(i > -1 && j > -1){
+            carry += num1.charAt(i) - 48 + num2.charAt(j) - 48;
             sb.append(carry % 10);
             carry /= 10;
             i--;
             j--;
         }
+        while (i > -1){
+            carry += num1.charAt(i) - 48;
+            sb.append(carry % 10);
+            carry /= 10;
+            i--;
+        }
+        while (j > -1){
+            carry += num2.charAt(j) - 48;
+            sb.append(carry % 10);
+            carry /= 10;
+            j--;
+        }
+        if (carry > 0) sb.append(carry);
         return sb.reverse().toString();
     }
 }
