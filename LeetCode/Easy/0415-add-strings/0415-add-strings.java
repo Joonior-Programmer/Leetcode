0c291c96adfc3bb17839e5b57eae02c701ae333f
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-03 22:59:20 +0900

    Time: 2 ms (92.14%), Space: 42.1 MB (90.74%) - LeetHub

--- LeetCode/Easy/0415-add-strings/0415-add-strings.java
+++ LeetCode/Easy/0415-add-strings/0415-add-strings.java
@@ -1,24 +1,26 @@
 class Solution {
     public String addStrings(String num1, String num2) {
         StringBuilder sb = new StringBuilder();
+        char[] num1Char = num1.toCharArray();
+        char[] num2Char = num2.toCharArray();
         int carry = 0;
         int i = num1.length() - 1;
         int j = num2.length() - 1;
         while(i > -1 && j > -1){
-            carry += num1.charAt(i) - 48 + num2.charAt(j) - 48;
+            carry += num1Char[i] - 48 + num2Char[j] - 48;
             sb.append(carry % 10);
             carry /= 10;
             i--;
             j--;
         }
         while (i > -1){
-            carry += num1.charAt(i) - 48;
+            carry += num1Char[i] - 48;
             sb.append(carry % 10);
             carry /= 10;
             i--;
         }
         while (j > -1){
-            carry += num2.charAt(j) - 48;
+            carry += num2Char[j] - 48;
             sb.append(carry % 10);
             carry /= 10;
             j--;
