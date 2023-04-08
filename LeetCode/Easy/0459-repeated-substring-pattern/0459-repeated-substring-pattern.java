03c2f65076d67717a93c1153055f767717675061
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-08 23:42:56 +0900

    Time: 2 ms (100.00%), Space: 43.5 MB (12.80%) - LeetHub

--- LeetCode/Easy/0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ LeetCode/Easy/0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,11 +1,20 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        char[] newS = new char[s.length() * 2 - 2];
-        char[] sCharArray = s.toCharArray();
-        int j = 0;
-        for (int i = 1; i < s.length(); ++i) newS[j++] = sCharArray[i];
-        for (int i = 0; i < s.length() - 1; ++i) newS[j++] = sCharArray[i];
+        // char[] newS = new char[s.length() * 2 - 2];
+        // char[] sCharArray = s.toCharArray();
+        // int j = 0;
+        // for (int i = 1; i < s.length(); ++i) newS[j++] = sCharArray[i];
+        // for (int i = 0; i < s.length() - 1; ++i) newS[j++] = sCharArray[i];
 
-        return new String(newS).contains(s);
+        // return new String(newS).contains(s);
+        int len = s.length();
+        for(int i = len / 2; i > 0; i--){
+            if (len % i == 0 && s.charAt(i-1) == s.charAt(len - 1)){
+                String part = s.substring(0, i);
+                int repeat = len / i;
+                if (part.repeat(repeat).equals(s)) return true;
+            }
+        }
+        return false;
     }
 }
