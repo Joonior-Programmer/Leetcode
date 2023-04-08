2555946af56e5dc66d7d2eee66c211c01f4ec3a4
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-08 23:33:35 +0900

    Time: 2427 ms (5.01%), Space: 42.8 MB (77.58%) - LeetHub

---
+++ LeetCode/Easy/0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        char[] newS = new char[s.length() * 2 - 2];
+        int j = 0;
+        for (int i = 1; i < s.length(); ++i) newS[j++] = s.charAt(i);
+        for (int i = 0; i < s.length() - 1; ++i) newS[j++] = s.charAt(i);
+        for (int i = 0; i < newS.length; ++i) System.out.println(newS[i]);
+
+        return new String(newS).contains(s);
+    }
+}
