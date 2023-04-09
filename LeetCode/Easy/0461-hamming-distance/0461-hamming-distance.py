5d2ede97db1b9e43d40f2d5b1dc37f54a8e3f25e
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-09 22:17:06 +0900

    Delete 0461-hamming-distance.py

--- LeetCode/Easy/0461-hamming-distance/0461-hamming-distance.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def hammingDistance(self, x: int, y: int) -> int:
-        ret = 0
-        while x or y:
-            if x & 1 != y & 1:
-                ret += 1
-            x >>= 1
-            y >>= 1
-        return ret
