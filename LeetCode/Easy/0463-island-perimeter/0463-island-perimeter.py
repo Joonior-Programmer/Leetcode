2d3cf9838c482fd48437826507997d599d3ef91d
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-10 22:35:15 +0900

    Time: 449 ms (95.09%), Space: 14.3 MB (61.96%) - LeetHub

--- LeetCode/Easy/0463-island-perimeter/0463-island-perimeter.py
+++ LeetCode/Easy/0463-island-perimeter/0463-island-perimeter.py
@@ -5,23 +5,11 @@ class Solution:
         c = len(grid[0]) - 1
         for row in range(len(grid)):
             for col in range(len(grid[0])):
-                temp = grid[row][col]
-                if temp != 1:
+                if grid[row][col] != 1:
                     continue
-                if row == 0:
-                    ret += 1
-                if row == r:
-                    ret += 1
-                if col == 0:
-                    ret += 1
-                if col == c:
-                    ret += 1
-                if row != 0 and grid[row - 1][col] != 1:
-                    ret += 1
-                if row != r and grid[row + 1][col] != 1:
-                    ret += 1
-                if col != 0 and grid[row][col - 1] != 1:
-                    ret += 1
-                if col != c and grid[row][col + 1] != 1:
-                    ret += 1
+                ret += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    ret -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    ret -= 2
         return ret
