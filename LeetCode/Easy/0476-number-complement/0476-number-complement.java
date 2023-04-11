d4ad0dfd7665c418af59d3b4f91a8c744da2404f
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-11 22:53:00 +0900

    Time: 0 ms (100.00%), Space: 39.6 MB (48.86%) - LeetHub

---
+++ LeetCode/Easy/0476-number-complement/0476-number-complement.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findComplement(int num) {
+        int ret = 0;
+        int temp = num;
+        while (temp > 0) {
+            ++ret;
+            temp >>= 1;
+        }
+        return num ^ (int)(Math.pow(2, ret) - 1);
+    }
+}
