8c975c0719373b141cf5b42d5b9fd7301672557a
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-11 22:50:45 +0900

    Delete 0476-number-complement.py

--- LeetCode/Easy/0476-number-complement/0476-number-complement.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findComplement(self, num: int) -> int:
-        ret = 0
-        temp = num
-        while temp:
-            ret += 1
-            temp >>= 1
-        return num ^ (2 ** ret - 1)
