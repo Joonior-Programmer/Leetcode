8d1a7d576fed4e41ed7e0ac1211bb273bff6c16a
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-13 00:54:36 +0900

    Time: 8 ms (54.39%), Space: 8.4 MB (47.70%) - LeetHub

--- LeetCode/Easy/0482-license-key-formatting/0482-license-key-formatting.cpp
+++ LeetCode/Easy/0482-license-key-formatting/0482-license-key-formatting.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
     string licenseKeyFormatting(string s, int k) {
-        int i = s.size() - 1;
         int temp = k;
-        stringstream ss;
-        while (i > -1) {
-            if (s[i] != '-'){
+        string ret;
+        for (int i = s.length()-1; i > -1; i--){
+            if (s[i] != '-') {
+                if (!temp) {
+                    ret += "-";
+                    temp = k;
+                }
                 if (s[i] >= 'a' && s[i] <= 'z'){
-                    if (temp == 0 && i > -1) {
-                        ss << '-';
-                        temp = k;
-                    }
-                    ss << (char) (s[i] - 32);
-                    i--;
-                    temp--;
-                    
+                    ret += s[i] - 32;
                 }
-                else {
-                    if (temp == 0 && i > -1) {
-                        ss << '-';
-                        temp = k;
-                    }
-                    ss << s[i];
-                    i--;
-                    temp--;
-                    
-                }
-                
+                else ret += s[i];
+                temp--;
             }
-            else i--;
             
         }
-        string ret = ss.str();
         reverse(ret.begin(), ret.end());
         return ret;
     }
