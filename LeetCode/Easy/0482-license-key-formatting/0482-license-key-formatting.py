635d39b42384b10e79c51bcf9135085a7779478c
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-12 23:12:54 +0900

    Delete 0482-license-key-formatting.py

--- LeetCode/Easy/0482-license-key-formatting/0482-license-key-formatting.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def licenseKeyFormatting(self, s: str, k: int) -> str:
-        s = s.replace("-", "").upper()[::-1]
-        ret = ""
-        for i in range(0, len(s), k):
-            ret += s[i:i+k] + "-"
-        ret = ret[:-1]
-        return ret[::-1]
