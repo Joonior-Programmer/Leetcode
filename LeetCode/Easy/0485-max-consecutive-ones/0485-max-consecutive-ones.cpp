052b0eee2676311e732f494888687f1ea936135e
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-14 22:47:37 +0900

    Time: 44 ms (37.54%), Space: 36.2 MB (23.67%) - LeetHub

--- LeetCode/Easy/0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ LeetCode/Easy/0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -5,12 +5,12 @@ public:
         int temp = 0;
         for (int i = 0; i < nums.size(); ++i){
             if (nums[i] == 0){
-                ret = max(ret, temp);
+                ret = temp > ret ? temp : ret;
                 temp = 0;
                 continue;
             }
             ++temp;
         }
-        return max(ret, temp);
+        return temp > ret ? temp : ret;
     }
 };
