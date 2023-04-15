2ee8586823ff8fc2e2109966b3c0359f78ec6dc6
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-15 23:01:00 +0900

    Delete 0492-construct-the-rectangle.go

--- LeetCode/Easy/0492-construct-the-rectangle/0492-construct-the-rectangle.go
+++
@@ -1,8 +0,0 @@
-func constructRectangle(area int) []int {
-    var w int= int(math.Sqrt(float64(area)))
-    for area % w != 0 {w--}
-    ret := make([]int, 2)
-    ret[0] = area/w
-    ret[1] = w
-    return ret
-}
