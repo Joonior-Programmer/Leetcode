3f52f401f663428238088d830689b4bb27b7981e
Joonior-Programmer <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-15 22:53:29 +0900

    Delete 0492-construct-the-rectangle.py

--- LeetCode/Easy/0492-construct-the-rectangle/0492-construct-the-rectangle.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def constructRectangle(self, area: int) -> List[int]:
-        for w in range(int(area ** 0.5), 0, -1):
-            if area % w == 0:
-                return [area // w, w]
