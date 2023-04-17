320db10396ccb766ae64c662011fa1c326b06200
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-17 23:34:51 +0900

    Time: 3 ms (91.44%), Space: 42.7 MB (61.22%) - LeetHub

--- LeetCode/Easy/0496-next-greater-element-i/0496-next-greater-element-i.java
+++ LeetCode/Easy/0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> findBig = new HashMap<>();
+        int[] findBig = new int[10000];
         Stack<Integer> stack = new Stack<>();
         int[] ret = new int[nums1.length];
-        
+
         for (int i = 0; i < nums2.length; ++i){
             while(!stack.empty() && stack.peek() < nums2[i]){
                 int key = stack.pop();
-                findBig.put(key, nums2[i]);
+                findBig[key] = nums2[i];
             }
             stack.push(nums2[i]);
         }
         
-        for (int i = 0; i < nums1.length; ++i) ret[i] = findBig.getOrDefault(nums1[i], -1);
+        for (int i = 0; i < nums1.length; ++i) ret[i] = findBig[nums1[i]] == 0 ? -1 : findBig[nums1[i]];
         
         return ret;
     }
