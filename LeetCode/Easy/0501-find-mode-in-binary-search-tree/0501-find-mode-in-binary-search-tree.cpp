7e440ffe43d6dc8a31f55257367850dfea7a3084
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-20 00:04:23 +0900

    Time: 66 ms (5.09%), Space: 25.4 MB (48.67%) - LeetHub

---
+++ LeetCode/Easy/0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, int> count;
+    vector<int> findMode(TreeNode* root) {
+        DFS(root);
+        int max_val = 0;
+        vector<int> ret;
+
+        for(auto elem: count){
+            cout << elem.first << " " << elem.second << endl;
+            if (elem.second > max_val) {
+                ret.clear();
+                ret.push_back(elem.first);
+                max_val = elem.second;
+            } else if (elem.second == max_val) ret.push_back(elem.first);
+        }
+        return ret;
+    }
+
+    void DFS(TreeNode* node){
+        if (!node) return;
+        if (count.find(node->val) == count.end()){
+            count[node->val] = 1;
+        } else ++count[node->val];
+        DFS(node->left);
+        DFS(node->right);
+    }
+};
