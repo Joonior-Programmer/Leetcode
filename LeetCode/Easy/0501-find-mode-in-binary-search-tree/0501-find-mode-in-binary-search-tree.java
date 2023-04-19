73ec3765845f5b64daed0e4e0e865d1b36909f18
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-19 23:50:20 +0900

    Time: 6 ms (37.18%), Space: 44.3 MB (32.78%) - LeetHub

---
+++ LeetCode/Easy/0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int[] findMode(TreeNode root) {
+        HashMap<Integer, Integer> count = new HashMap<>();
+        
+        DFS(root, count);
+        
+        int max_val = 0;
+        int val_count = 0;
+        for (int value: count.values()) {
+            if (value > max_val){
+                max_val = value;
+                val_count = 1;
+            } else if (max_val == value) ++val_count;
+        }
+        int[] ret = new int[val_count];
+        int i = 0;
+        for (int key: count.keySet()) {
+            if (count.get(key) == max_val){
+                ret[i++] = key;
+            }
+        }
+        
+        return ret;
+    }
+    
+    private void DFS(TreeNode node, HashMap<Integer, Integer> count){
+        if (node == null) return;
+        count.put(node.val, count.getOrDefault(node.val, 0) + 1);
+        DFS(node.left, count);
+        DFS(node.right, count);
+    }
+}
