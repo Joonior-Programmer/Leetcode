af986606b16daae429e2330025e4d96ae0196087
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-23 22:34:38 +0900

    Delete 0507-perfect-number.py

--- LeetCode/Easy/0507-perfect-number/0507-perfect-number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def checkPerfectNumber(self, num: int) -> bool:
-        if num % 2 != 0 or num < 3:
-            return False
-        ret = 1
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                ret += i + (num // i)
-        return ret == num
