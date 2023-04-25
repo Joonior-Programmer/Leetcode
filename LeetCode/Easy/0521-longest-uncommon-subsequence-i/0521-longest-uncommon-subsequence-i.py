7a62b509a11efc6cbbe7e69990842cca25f3c740
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-25 23:03:11 +0900

    Time: 36 ms (19.14%), Space: 13.8 MB (92.67%) - LeetHub

--- LeetCode/Easy/0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.py
+++ LeetCode/Easy/0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.py
@@ -1,7 +1,7 @@
 class Solution:
     def findLUSlength(self, a: str, b: str) -> int:
-        if len(a) == len(b) and (a in b or b in a):
+        if a == b:
             return -1
-        return len(a) if len(a) > len(b) else len(b)
+        return max(len(a), len(b))
         
                     
