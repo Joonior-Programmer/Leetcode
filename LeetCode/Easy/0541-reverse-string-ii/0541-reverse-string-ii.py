0129bedee09db3de46cf6e4f43a2c21b2b079efa
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-04-27 22:54:00 +0900

    Time: 49 ms (6.24%), Space: 16.3 MB (11.03%) - LeetHub

--- LeetCode/Easy/0541-reverse-string-ii/0541-reverse-string-ii.py
+++ LeetCode/Easy/0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -2,18 +2,11 @@ class Solution:
     def reverseStr(self, s: str, k: int) -> str:
         ret = ""
         is_reverse = True
-        for i in range(k-1, len(s), k):
+        for i in range(0, len(s), k):
             if is_reverse:
-                for j in range(i, i-k, -1):
-                    ret += s[j]
+                ret += s[i:i+k][::-1]
             else:
-                for j in range(i-k+1, i+1):
-                    ret += s[j]
+                ret += s[i:i+k]
             is_reverse = not is_reverse
 
-        if len(s) % k != 0:
-            if is_reverse:
-                ret += s[len(s) // k * k:][::-1]
-            else:
-                ret += s[len(s) // k * k:]
         return ret
