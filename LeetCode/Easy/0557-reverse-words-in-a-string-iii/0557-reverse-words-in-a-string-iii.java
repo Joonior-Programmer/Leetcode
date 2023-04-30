f1b2e2c010247aa2b70dada8504b4982648ae92b
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-05-01 00:49:37 +0900

    Time: 4 ms (98.35%), Space: 43.2 MB (52.80%) - LeetHub

--- LeetCode/Easy/0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ LeetCode/Easy/0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,15 +1,44 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] splitted = s.split(" ");
-        StringBuffer sb = new StringBuffer();
-        StringBuffer temp = new StringBuffer();
-        for (String v : splitted){
-            temp.append(v);
-            temp.reverse();
-            sb.append(temp.toString() + " ");
-            temp.setLength(0);
+        // String[] splitted = s.split(" ");
+        // StringBuffer sb = new StringBuffer();
+        // StringBuffer temp = new StringBuffer();
+        // for (String v : splitted){
+        //     temp.append(v);
+        //     temp.reverse();
+        //     sb.append(temp.toString() + " ");
+        //     temp.setLength(0);
+        // }
+        // sb.setLength(sb.length() - 1);
+        // return sb.toString();
+        char[] sCharArray = s.toCharArray();
+        int j = 0;
+        char temp;
+        int l;
+        int r;
+        for (int i = 1; i < sCharArray.length; ++i){
+            if (sCharArray[i] == ' '){
+                r = i - 1;
+                l = j;
+                while (l < r){
+                    temp = sCharArray[l];
+                    sCharArray[l] = sCharArray[r];
+                    sCharArray[r] = temp;
+                    ++l;
+                    r--;
+                }
+                j = i + 1;
+            }
         }
-        sb.setLength(sb.length() - 1);
-        return sb.toString();
+        r = sCharArray.length - 1;
+        l = j;
+        while (l < r){
+            temp = sCharArray[l];
+            sCharArray[l] = sCharArray[r];
+            sCharArray[r] = temp;
+            ++l;
+            r--;
+        }
+        return new String(sCharArray);
     }
 }
