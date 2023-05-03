d8f702e8710afbb28d47a728046071f77d251b33
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-05-03 23:03:51 +0900

    Time: 188 ms (44.90%), Space: 16.9 MB (9.53%) - LeetHub

--- LeetCode/Easy/2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ LeetCode/Easy/2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -2,15 +2,5 @@ class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
         is_exist_1 = set(nums1)
         is_exist_2 = set(nums2)
-        
-        ret = [[],[]]
-
-        for num in is_exist_1:
-            if num not in is_exist_2:
-                ret[0].append(num)
                 
-        for num in is_exist_2:
-            if num not in is_exist_1:
-                ret[1].append(num)
-                
-        return ret
+        return [is_exist_1.difference(is_exist_2), is_exist_2.difference(is_exist_1)]
