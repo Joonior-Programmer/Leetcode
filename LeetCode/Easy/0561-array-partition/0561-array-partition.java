02b5a40eeb52aad15a3c89cbf07be20d5dfeac33
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-05-07 23:34:15 +0900

    Time: 6 ms (98.33%), Space: 44.7 MB (58.06%) - LeetHub

--- LeetCode/Easy/0561-array-partition/0561-array-partition.java
+++ LeetCode/Easy/0561-array-partition/0561-array-partition.java
@@ -1,8 +1,19 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
+        countingSort(nums);
         int ret = 0;
         for (int i = 0; i < nums.length; i = i + 2) ret += nums[i];
         return ret;
+    }
+    
+    private void countingSort(int[] nums){
+        int[] temp = new int[20001];
+        int idx = 0;
+        for (int i = 0; i < nums.length; ++i) ++temp[nums[i]+10000];
+        
+        for (int i = 0; i < 20001; ++i){
+            for (int j = 0; j < temp[i]; ++j) nums[idx++] = i-10000;
+        }
+
     }
 }
