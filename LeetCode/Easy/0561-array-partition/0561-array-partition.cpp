2f1a56e4896676cb411502670ee93b5efc413756
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-05-07 23:10:26 +0900

    Delete 0561-array-partition.cpp

--- LeetCode/Easy/0561-array-partition/0561-array-partition.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int arrayPairSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int ret = 0;
-        for (int i = 0; i < nums.size(); i = i + 2) ret += nums[i];
-        return ret;
-    }
-};
