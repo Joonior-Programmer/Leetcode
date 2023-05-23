a8aed80719ee22262d82653d5ee423b786dedc33
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-05-23 23:30:14 +0900

    Time: 45 ms (60.67%), Space: 16.3 MB (55.10%) - LeetHub

--- LeetCode/Easy/1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ LeetCode/Easy/1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,13 +1,15 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         count = [0] * 2000
-        ret = set()
+        ret = [0] * 1001
         for v in arr:
             count[v+1000] += 1
         
         for v in count:
-            if v != 0 and v in ret:
+            if v == 0:
+                continue
+            if ret[v] != 0:
                 return False
-            ret.add(v)
+            ret[v] += 1
             
         return True
