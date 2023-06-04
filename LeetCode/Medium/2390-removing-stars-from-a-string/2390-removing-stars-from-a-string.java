8b820c8cc08862c15c7908c0cd41e9e3e7606001
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-06-05 00:11:11 +0900

    Time: 14 ms (99.40%), Space: 45 MB (29.10%) - LeetHub

--- LeetCode/Medium/2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ LeetCode/Medium/2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,11 +1,13 @@
 class Solution {
     public String removeStars(String s) {
-        StringBuilder sb = new StringBuilder();
-
-        for (char v : s.toCharArray()){
-            if (v != '*') sb.append(v);
-            else if (sb.length() > 0) sb.setLength(sb.length() - 1);
+        int i = s.indexOf("*");
+        if (i < 0) return s;
+        char[] ret = s.toCharArray();
+        
+        for (int j = i; j < ret.length; ++j){
+            if (ret[j] != '*') ret[i++] = ret[j];
+            else if (i > 0) i--;
         }
-        return sb.toString();
+        return new String(ret,0 , i);
     }
 }
