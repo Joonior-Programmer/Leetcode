e42e28839d9e38ce36fc42ca55d8e9642c5d09e5
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-06-05 23:41:13 +0900

    Time: 101 ms (86.77%), Space: 16.9 MB (25.89%) - LeetHub

--- LeetCode/Easy/0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ LeetCode/Easy/0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,6 +1,5 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        ok = True
         for i in range(0, len(s) // 2):
             if s[i] != s[-(i+1)]:
                 return s[:i] + s[i+1:] == (s[:i] + s[i+1:])[::-1] or s[:len(s)-i-1]+s[len(s)-i:] == (s[:len(s)-i-1]+s[len(s)-i:])[::-1]
