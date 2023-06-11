821bf2bdc5e4150fd2d0fb3632067f9409c550b2
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-06-12 00:14:23 +0900

    Time: 112 ms (81.36%), Space: 12.9 MB (84.91%) - LeetHub

--- LeetCode/Medium/1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.go
+++ LeetCode/Medium/1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.go
@@ -7,24 +7,24 @@
  * }
  */
 func longestZigZag(root *TreeNode) int {
-    var ret int = 0
-    DFS(root, true, 0, &ret)
-    DFS(root, false, 0, &ret)
+    ret := 0
+    DFS(root, 0, 0, &ret)
     return ret - 1
 }
 
-func DFS(node *TreeNode, is_left bool, depth int, ret *int) {
-    if node == nil {
-        if *ret < depth {
-            *ret = depth
-        }
-        return
+func DFS(node *TreeNode, left int, right int, ret *int) {
+    if node == nil { 
+        *ret = max(*ret, max(left, right))
+        return 
     }
-    if is_left {
-        DFS(node.Left, false, depth+1, ret)
-        DFS(node.Right, false, 0, ret)
-    } else {
-        DFS(node.Right, true, depth+1, ret)
-        DFS(node.Left, true, 0, ret)
+
+    DFS(node.Left, right + 1, 0, ret)
+    DFS(node.Right, 0, left + 1, ret)
+}
+
+func max(a int, b int) int {
+    if a > b {
+        return a
     }
+    return b
 }
