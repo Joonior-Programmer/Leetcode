3dfb2cc2f3a1dc2d578ccd53403451f233dad6bd
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-06-12 00:24:18 +0900

    Time: 6 ms (99.48%), Space: 56.4 MB (5.13%) - LeetHub

--- LeetCode/Medium/1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
+++ LeetCode/Medium/1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
@@ -16,17 +16,15 @@
 class Solution {
     int ret = 0;
      public int longestZigZag(TreeNode root) {
-        DFS(root, true, 0);
-        DFS(root, false, 0);
-        return this.ret - 1;
+        DFS(root, 0, 0);
+        return this.ret;
     }
     
-    private void DFS(TreeNode node, boolean is_left, int ret){
-        if (node == null) {
-            this.ret = Math.max(ret, this.ret);
-            return;
-        }
-        DFS(node.left, !is_left, is_left ? ret + 1 : 0);
-        DFS(node.right, !is_left, is_left ? 0 : ret + 1);
+    private void DFS(TreeNode node, int left, int right){
+        if (node == null) return;
+        
+        ret = Math.max(ret, Math.max(left, right));
+        DFS(node.left, right + 1, 0);
+        DFS(node.right, 0, left + 1);
     }
 }
