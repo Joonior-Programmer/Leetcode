d41cb0226b31d0ad43ecc09e2343d1eb76247300
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-06-17 00:06:56 +0900

    Time: 0 ms (100.00%), Space: 39.1 MB (83.41%) - LeetHub

--- LeetCode/Medium/1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
+++ LeetCode/Medium/1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -1,15 +1,5 @@
 class Solution {
     public int minFlips(int a, int b, int c) {
-        int ret = 0;
-        while (a > 0 || b > 0 || c > 0){
-            if ((c & 1) == 0) 
-            ret += (a & 1) + (b & 1);
-            else if (((a & 1) | (b & 1)) != 1) ++ret;
-            a>>=1;
-            b>>=1;
-            c>>=1;
-        }
-
-        return ret;
+        return Integer.bitCount((a | b) ^ c) + Integer.bitCount((a & b) & ~c);
     }
 }
