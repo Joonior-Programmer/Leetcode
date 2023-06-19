f6ed9bb015e9cf35bd206c516573260d6537ef38
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-06-19 23:51:03 +0900

    Time: 82 ms (81.48%), Space: 20.8 MB (9.46%) - LeetHub

--- LeetCode/Medium/0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ LeetCode/Medium/0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -6,54 +6,37 @@
 #         self.right = right
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        prev = None
-        curr = root
+        prev = head = TreeNode()
+        head.left = curr = root
         is_left = True
-        while curr:
-            if curr.val == key:
-                is_root_change = False
-                if not prev:
-                    if root.right:
-                        root = root.right
-                    else:
-                        root = root.left
-                    is_root_change = True
-                    prev = root
-                
-                if not prev:
-                    return None
 
-                temp = None
-                new_position = None 
-
-                if curr.right:
-                    new_position = curr.right
-                    temp = curr.left
-                else:
-                    new_position = curr.left
-                
-                if not is_root_change:
-                    if is_left:
-                        prev.left = new_position
-                    else:
-                        prev.right = new_position
-
-                if not new_position:
-                    break
-                    
-                while new_position.left:
-                    new_position = new_position.left
-                
-                new_position.left = temp
-                break
-
-            elif curr.val > key:
-                prev = curr
+        while curr and curr.val != key:
+            prev = curr
+            if curr.val > key:
                 curr = curr.left
                 is_left = True
             else:
-                prev = curr
                 curr = curr.right
                 is_left = False
-        # print(prev, curr)
-        return root
+        
+        if not curr:
+            return root
+        
+        if not curr.right:
+            if is_left:
+                prev.left = curr.left
+            else:
+                prev.right = curr.left
+        else:
+            temp = curr.right
+
+            while temp.left:
+                temp = temp.left
+
+            temp.left = curr.left
+            if is_left:
+                prev.left = curr.right
+            else:
+                prev.right = curr.right
+
+        return head.left
