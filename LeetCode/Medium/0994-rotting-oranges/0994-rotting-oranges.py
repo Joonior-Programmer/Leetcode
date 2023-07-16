29b95047d9886c45fcf3467fdc12f4df3cdb3da8
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-07-16 23:50:06 +0900

    Time: 53 ms (98.81%), Space: 16.4 MB (27.43%) - LeetHub

--- LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.py
+++ LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,7 +1,8 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        ret = -1
+        ret = 0
         fresh_count = 0
+        rotten_count = 0
         q = []
         row_len = len(grid) - 1
         col_len = len(grid[0]) - 1
@@ -9,39 +10,43 @@ class Solution:
         for row in range(row_len + 1):
             for col in range(col_len + 1):
                 if grid[row][col] == 2:
+                    rotten_count += 1
                     q.append([row, col])
                 elif grid[row][col] == 1:
                     fresh_count += 1 
 
-        if not q and fresh_count:
-            return -1
-        elif not q:
+        total = rotten_count + fresh_count
+        
+        if total == rotten_count:
             return 0
-
+        
         while q:
             temp_q = []
-            while q:
-                row, col = q.pop()
+
+            for i in range(len(q)):
+                row, col = q[i][0], q[i][1]
+                
 
                 if row > 0 and grid[row-1][col] == 1:
+                    rotten_count += 1
                     temp_q.append([row-1, col])
                     grid[row-1][col] = 2
                 if row < row_len and grid[row+1][col] == 1:
+                    rotten_count += 1
                     temp_q.append([row+1, col])
                     grid[row+1][col] = 2
                 if col > 0 and grid[row][col-1] == 1:
+                    rotten_count += 1
                     temp_q.append([row, col-1])
                     grid[row][col-1] = 2
                 if col < col_len and grid[row][col+1] == 1:
+                    rotten_count += 1
                     temp_q.append([row, col+1])
                     grid[row][col+1] = 2
 
-            q = temp_q
             ret += 1
-        
-        for row in range(row_len + 1):
-            for col in range(col_len + 1):
-                if grid[row][col] == 1:
-                    return -1
+            if rotten_count == total:
+                return ret
+            q = temp_q
 
-        return ret
+        return -1
