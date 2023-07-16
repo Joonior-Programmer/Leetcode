309916e97167e9fa044b46c6d7eb3a327418f9a8
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-07-16 23:20:49 +0900

    Time: 1 ms (100.00%), Space: 41.6 MB (43.91%) - LeetHub

--- LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.java
+++ LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,60 +1,59 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int ret = -1;
-        boolean isFreshExist = false;
+        int ret = 0;
+        int fresh = 0;
+        int rotten = 0;
         Queue<int[]> q = new LinkedList();
         int rowLen = grid.length - 1;
         int colLen = grid[0].length - 1;
         
         for (int i = 0; i < rowLen + 1; ++i){
             for (int j = 0; j < colLen + 1; ++j){
-                if (grid[i][j] == 2) q.offer(createCoord(i, j));
-                else if (grid[i][j] == 1) isFreshExist = true;
+                if (grid[i][j] == 2) {
+                    q.offer(createCoord(i, j));
+                    ++rotten;
+                }
+                else if (grid[i][j] == 1) ++fresh;
             }
         }
         
-        if (q.isEmpty()){
-            if (isFreshExist) return ret;
-            return 0;
-        }
+        int total = fresh + rotten;
+
+        if (total == rotten) return 0;
         
         while (!q.isEmpty()){
-            Queue<int[]> tempQ = new LinkedList();
-            
-            while (!q.isEmpty()){
+            int repeat = q.size();
+            for (int i = 0; i < repeat; ++i){
                 int[] coord = q.poll();
                 int row = coord[0];
                 int col = coord[1];
 
                 if (row > 0 && grid[row-1][col] == 1) {
-                    tempQ.offer(createCoord(row-1, col));
+                    ++rotten;
+                    q.offer(createCoord(row-1, col));
                     grid[row-1][col] = 2;
                 } 
                 if (row < rowLen && grid[row+1][col] == 1){
-                    tempQ.offer(createCoord(row+1, col));
+                    ++rotten;
+                    q.offer(createCoord(row+1, col));
                     grid[row+1][col] = 2;
                 } 
                 if (col > 0 && grid[row][col-1] == 1){
-                    tempQ.offer(createCoord(row, col-1));
+                    ++rotten;
+                    q.offer(createCoord(row, col-1));
                     grid[row][col-1] = 2;
                 } 
                 if (col < colLen && grid[row][col+1] == 1){
-                    tempQ.offer(createCoord(row, col+1));
+                    ++rotten;
+                    q.offer(createCoord(row, col+1));
                     grid[row][col+1] = 2;
                 }
             }
-
-            q = tempQ;
             ++ret;
+            if (rotten == total) return ret;
         }
 
-        for (int i = 0; i < rowLen + 1; ++i){
-            for (int j = 0; j < colLen + 1; ++j){
-                if (grid[i][j] == 1) return -1;
-            }
-        }
-        
-        return ret;
+        return -1;
     }
     
     private int[] createCoord(int row, int col){
