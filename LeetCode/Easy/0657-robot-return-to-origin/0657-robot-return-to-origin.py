9cdb6811b70bf89fc4d5e0eb29f92f3e31abb3cc
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-08-01 23:47:53 +0900

    Time: 48 ms (91.81%), Space: 16.5 MB (64.61%) - LeetHub

---
+++ LeetCode/Easy/0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -0,0 +1,16 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x = 0
+        y = 0
+        
+        for move in moves:
+            if move == "U":
+                y += 1
+            elif move == "D":
+                y -= 1
+            elif move == "R":
+                x += 1
+            else:
+                x -= 1
+        
+        return not x and not y
