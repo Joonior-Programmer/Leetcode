172fc605b84b30d7636ffe99852e705a874a315a
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-08-15 01:24:44 +0900

    Time: 14 ms (93.19%), Space: 17.8 MB (44.20%) - LeetHub

---
+++ LeetCode/Hard/0135-candy/0135-candy.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int ret = 0;
+        vector<int> candies(n, 1);
+        
+        for (int i = 1; i < n; ++i) if (ratings[i] > ratings[i-1] && candies[i] <= candies[i-1]) candies[i] = candies[i-1] + 1;
+        
+        for (int i = n - 2; i > -1; i--) if (ratings[i] > ratings[i+1] && candies[i] <= candies[i+1]) candies[i] = candies[i+1] + 1;
+        
+        for (int i = 0; i < n; ++i) ret += candies[i];
+        
+        return ret;
+    }
+};
