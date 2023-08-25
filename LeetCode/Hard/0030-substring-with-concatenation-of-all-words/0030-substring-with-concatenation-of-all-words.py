fcb865bfef944af9f5c90568d19c7fc99fd7d101
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-08-26 00:25:03 +0900

    Time: 72 ms (98.93%), Space: 17.2 MB (22.96%) - LeetHub

--- LeetCode/Hard/0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ LeetCode/Hard/0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,32 +1,35 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         n = len(s)
-        length = len(words[0])
-        l = 0
-        r = length * len(words)
+        word_length = len(words[0])
+        num_of_word = len(words)
+        total_length = num_of_word * word_length
         ret = []
-        temp = Counter(words)
 
-        while r < n+1:
-            temp_count = temp.copy()
-            condition = True
- 
-            for i in range(l, r, length):
-                if s[i:i+length] not in temp_count:
-                    condition = False
-                    break
-                temp_count[s[i:i+length]] -= 1
-            
-            if condition:
-                for v in temp_count.values():
-                    if v != 0:
-                        condition = False
-                        break
+        count = Counter(words)
+
+        for i in range(word_length):
+            l = r = i
+            temp_count = defaultdict(int)
+
+            while r + word_length <= n:
+                temp_word = s[r:r+word_length]
+                r += word_length
                 
-                if condition:
-                    ret.append(l)
+                if temp_word in count:
+                    temp_count[temp_word] += 1
+                
+                    while temp_count[temp_word] > count[temp_word]:
+                        temp_count[s[l:l+word_length]] -= 1
+                        l += word_length
 
-            l += 1
-            r += 1
+                    if r - l == total_length:
+                        ret.append(l)
+                        temp_count[s[l:l + word_length]] -= 1
+                        l += word_length
+                
+                else:
+                    temp_count.clear()
+                    l = r
 
         return ret
