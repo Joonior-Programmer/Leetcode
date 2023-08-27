fd78e2c9eaeeb4792e8aefbf0ec8f283bf2339f2
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-08-27 23:10:25 +0900

    Time: 7 ms (99.46%), Space: 18 MB (80.43%) - LeetHub

---
+++ LeetCode/Medium/0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int row[9][9]{0};
+        int col[9][9]{0};
+        int box[9][9]{0};
+        
+        for (int r = 0; r < 9; ++r){
+            for (int c = 0; c < 9; ++c){
+                int v = board[r][c];
+                
+                if (v == '.') continue;
+
+                v -= 49;
+
+                int boxNum = (c < 3 ? 0 : c < 6 ? 1 : 2) + (3 * (r < 3 ? 0 : r < 6 ? 1 : 2));
+
+                if (row[r][v] > 0 || col[c][v] > 0 || box[boxNum][v] > 0) return false;
+
+                ++row[r][v];
+                ++col[c][v];
+                ++box[boxNum][v];
+            }
+        }
+        
+        return true;
+    }
+};
