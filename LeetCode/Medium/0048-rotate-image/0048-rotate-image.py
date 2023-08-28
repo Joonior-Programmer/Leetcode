44867ea480733205fc0528579b98b091c3168962
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-08-29 00:22:05 +0900

    Time: 36 ms (94.37%), Space: 16.4 MB (52.38%) - LeetHub

--- LeetCode/Medium/0048-rotate-image/0048-rotate-image.py
+++ LeetCode/Medium/0048-rotate-image/0048-rotate-image.py
@@ -4,29 +4,6 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix)
-        repeat = n // 2
-        temp = [0] * (n - 1)
-        i = 0
-        while i < repeat:
+        for i in range(n//2):
             for j in range(i, n-i-1):
-                temp[j-i] = matrix[j][n-i-1]
-                matrix[j][n-i-1] = matrix[i][j]
-
-            for j in range(n-i-1, i, -1):
-                temp_value = matrix[n-i-1][j]
-                matrix[n-i-1][j] = temp[n-j-i-1]
-                temp[n-j-i-1] = temp_value
-
-
-            for j in range(n-i-1, i, -1):
-                temp_value = matrix[j][i]
-                matrix[j][i] = temp[n-j-i-1]
-                temp[n-j-i-1] = temp_value
-            
-            for j in range(i, n-i-1):
-                matrix[i][j] = temp[j-i]
-
-            i += 1
-            
-            
-        
+                matrix[i][j], matrix[j][n-i-1], matrix[n-i-1][n-j-1], matrix[n-j-1][i] = matrix[n-j-1][i], matrix[i][j], matrix[j][n-i-1], matrix[n-i-1][n-j-1]
