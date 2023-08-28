b6fa5803e6b0dcf4b1684501fa59c8a8aa8a8458
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-08-29 00:12:51 +0900

    Time: 0 ms (100.00%), Space: 41.1 MB (85.19%) - LeetHub

---
+++ LeetCode/Medium/0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int repeat = n / 2;
+        int i = 0;
+        while (i < repeat){
+            for (int j = i; j < n-i-1; ++j){
+                int topLeft = matrix[i][j];
+                int topRight = matrix[j][n-i-1];
+                int botRight = matrix[n-i-1][n-j-1];
+                int botLeft = matrix[n-j-1][i];
+
+                matrix[i][j] = botLeft;
+                matrix[j][n-i-1] = topLeft;
+                matrix[n-i-1][n-j-1] = topRight;
+                matrix[n-j-1][i] = botRight;
+            }
+            ++i;
+        }
+    }
+}
