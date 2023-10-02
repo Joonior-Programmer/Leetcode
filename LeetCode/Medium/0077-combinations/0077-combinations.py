bed7e2a1ffda0a872694ae8209f32cbceb330a64
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-10-02 23:20:41 +0900

    Time: 836 ms (79.88%), Space: 64.8 MB (64.70%) - LeetHub

--- LeetCode/Medium/0077-combinations/0077-combinations.py
+++ LeetCode/Medium/0077-combinations/0077-combinations.py
@@ -2,16 +2,16 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         ret = []
 
-        def helper(curr, val):
-            if len(curr) == k:
+        def helper(curr, val, remained):
+            if not remained:
                 ret.append(curr.copy())
                 return
                 
-            for i in range(val, n + 1):
+            for i in range(val, n - remained + 2):
                 curr.append(i)
-                helper(curr, i + 1)
+                helper(curr, i + 1, remained - 1)
                 curr.pop()
                 
-        helper([], 1)
+        helper([], 1, k)
 
         return ret
