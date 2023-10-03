2899380a9eefb74ce4630459af062f363a5a067f
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-10-03 23:57:56 +0900

    Time: 2349 ms (88.22%), Space: 16.3 MB (86.38%) - LeetHub

--- LeetCode/Medium/0079-word-search/0079-word-search.py
+++ LeetCode/Medium/0079-word-search/0079-word-search.py
@@ -2,37 +2,32 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         row_len = len(board) - 1
         col_len = len(board[0]) - 1
-
-        visited = set()
         
         def dfs(row, col, idx):
-            if idx == len(word):
-                return True
-            if board[row][col] != word[idx]:
-                return False
-
             idx += 1
-
+            
             if idx == len(word):
                 return True
 
-            visited.add((row, col))
+            temp = board[row][col]
+            board[row][col] = "?"
 
-            if row > 0 and (row-1, col) not in visited and dfs(row-1, col, idx):
+            if row > 0 and board[row-1][col] == word[idx] and dfs(row-1, col, idx):
                 return True
-            if row < row_len and (row+1, col) not in visited and dfs(row+1, col, idx):
+            if row < row_len and board[row+1][col] == word[idx] and dfs(row+1, col, idx):
                 return True
-            if col > 0 and (row, col-1) not in visited and dfs(row, col-1, idx):
+            if col > 0 and board[row][col-1] == word[idx] and dfs(row, col-1, idx):
                 return True
-            if col < col_len and (row, col+1) not in visited and dfs(row, col+1, idx):
+            if col < col_len and board[row][col+1] == word[idx] and dfs(row, col+1, idx):
                 return True
             
-            visited.remove((row,col))
+            board[row][col] = temp
+            
             return False
         
         for row in range(row_len+1):
             for col in range(col_len+1):
-                if dfs(row, col, 0):
+                if board[row][col] == word[0] and dfs(row, col, 0):
                     return True
         
 
