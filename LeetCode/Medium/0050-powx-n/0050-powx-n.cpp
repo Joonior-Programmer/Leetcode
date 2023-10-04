eb651a07d3ff52257f721c803c715a3013b92483
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-10-05 00:16:39 +0900

    Time: 0 ms (100.00%), Space: 6.3 MB (16.51%) - LeetHub

---
+++ LeetCode/Medium/0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long tempN = n;
+        if (n < 0){
+            x = 1 / x;
+            tempN *= -1;
+        }
+        
+        return helper(x, n);
+    }
+    
+    double helper(double x, long n){
+        if (n == 0) return 1;
+        
+        double half = helper(x, n / 2);
+        
+        if ((n & 1) == 1) return x * half * half;
+        return half * half;
+    }
+};
