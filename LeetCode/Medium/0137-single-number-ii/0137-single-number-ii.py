a008714057e49501cc869b39e0e77bdfa3b03962
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-10-09 23:49:50 +0900

    Time: 59 ms (79.61%), Space: 17.9 MB (86.71%) - LeetHub

--- LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.py
+++ LeetCode/Medium/0137-single-number-ii/0137-single-number-ii.py
@@ -1,15 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ret = set()
-        container = set()
-        
-        for v in nums:
-            if v not in container:
-                container.add(v)
-                ret.add(v)
-            else:
-                if v in ret:
-                    ret.remove(v)
-                
-        for v in ret:
-            return v
+        ones=0
+        threes=0
+        for i in nums:
+            ones=(ones^i)&(~threes)
+            threes=(threes^i)&(~ones)
+        return ones
