473d6f046e2f632bf7489cfe00d2fbeb3c8bd036
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-11-09 23:35:33 +0900

    Time: 45 ms (93.38%), Space: 14.9 MB (62.11%) - LeetHub

--- LeetCode/Medium/0322-coin-change/0322-coin-change.cpp
+++ LeetCode/Medium/0322-coin-change/0322-coin-change.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1);
+        vector<int> dp(amount+1, amount + 1);
+        dp[0] = 0;
         
-        for (int i = 1; i < amount + 1; ++i){
-            dp[i] = amount + 1;
-            for (int coin : coins){
-                if (i - coin >= 0) dp[i] = min(dp[i], 1 + dp[i - coin]);
+        for (int coin : coins){
+            for (int i = coin; i < amount + 1; ++i){    
+                dp[i] = min(dp[i], 1 + dp[i - coin]);
             }
         }
         
