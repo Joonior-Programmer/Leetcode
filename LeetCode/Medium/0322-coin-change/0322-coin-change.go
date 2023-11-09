fef44cf2d266a1c14b5951cb29ab7b443ff35e51
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-11-09 23:37:31 +0900

    Time: 2 ms (99.66%), Space: 6.9 MB (45.78%) - LeetHub

--- LeetCode/Medium/0322-coin-change/0322-coin-change.go
+++ LeetCode/Medium/0322-coin-change/0322-coin-change.go
@@ -1,12 +1,11 @@
 func coinChange(coins []int, amount int) int {
     dp := make([]int, amount + 1)
     
-    for i := 1; i < amount + 1; i++ {
-        dp[i] = amount + 1
-        for _, coin := range coins {
-            if i - coin >= 0 {
-                dp[i] = min(dp[i], 1 + dp[i - coin])
-            }
+    for i := 1; i < amount + 1; i++ { dp[i] = amount + 1 }
+    
+    for _, coin := range coins {
+        for i := coin; i < amount + 1; i++ {
+            dp[i] = min(dp[i], 1 + dp[i - coin])
         }
     }
     
