492b7460d4ae5135e6b08e1cc3ade4f519e883d8
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-11-23 00:18:55 +0900

    Time: 0 ms (100.00%), Space: 6.2 MB (98.42%) - LeetHub

--- LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.cpp
+++ LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     int reverse(int x) {
-        if (x == 0) return 0;
-        
-        bool minus = x < 0;
         long ret = 0;
-        long newX = x;
-        
-        if (minus) newX *= -1;
-        
-        while (newX % 10 == 0) newX /= 10;
-        
-        while (newX > 0){
-            ret = ret * 10 + newX % 10;
-            newX /= 10;
+                
+        while (x != 0){
+            cout << x % 10 << endl;
+            ret = ret * 10 + x % 10;
+            x /= 10;
         }
-        
-        if (minus) ret *= -1;
         
         if (ret < pow(-2, 31) || ret > pow(2, 31) - 1) return 0;
         
