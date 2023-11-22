75d6851058f815115dd50c8dfee2bf6bc93f8088
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-11-23 00:20:12 +0900

    Time: 1 ms (97.59%), Space: 40.3 MB (7.19%) - LeetHub

--- LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.java
+++ LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.java
@@ -1,23 +1,15 @@
 class Solution {
     public int reverse(int x) {
-        if (x == 0) return 0;
-        
-        boolean minus = x < 0;
         long ret = 0;
-        
-        if (minus) x *= -1;
-        
-        while (x % 10 == 0) x /= 10;
-        
-        while (x > 0){
+
+        while (x != 0){
             ret = ret * 10 + x % 10;
             x /= 10;
+            
         }
         
-        if (minus) ret *= -1;
-        
         if (ret < Math.pow(-2, 31) || ret > Math.pow(2, 31) - 1) return 0;
-        
+    
         return (int) ret;
     }
 }
