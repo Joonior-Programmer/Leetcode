92e7c5f921fd88256f039aad34136f041d2b17cd
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2023-12-09 22:25:34 +0900

    Time: 0 ms (100.00%), Space: 21 MB (98.01%) - LeetHub

---
+++ LeetCode/Easy/2951-find-the-peaks/2951-find-the-peaks.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        vector<int> ret;
+        
+        for (int i = 1; i < mountain.size()-1; ++i)
+            if (mountain[i] > mountain[i-1] && mountain[i] > mountain[i+1]) ret.push_back(i);
+        
+        return ret;
+    }
+};
