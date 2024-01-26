80be4701692974c5aa7c7bcf9c54e436c55243a9
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2024-01-26 22:13:24 +0900

    Time: 0 ms (100.00%), Space: 7.2 MB (7.50%) - LeetHub

---
+++ LeetCode/Easy/1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int sumBase(int n, int k) {
+        int ret = 0;
+        int i = 7;
+        
+        while (n < pow(k, i))
+            i--;
+
+        while (i > 0){
+            int temp = (int) pow(k, i);
+            ret += n / temp;
+            n -= n / temp * temp;
+            i--;
+        }
+        
+        return ret + n;
+    }
+};
