86de7d0f0308393313f5b847ce67ff6f40fa1abb
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2024-02-01 22:27:40 +0900

    Time: 21 ms (97.20%), Space: 56.5 MB (31.20%) - LeetHub

--- LeetCode/Medium/2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
+++ LeetCode/Medium/2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
@@ -7,24 +7,19 @@ class Solution {
         for (int i = 0; i < nums.length; ++i) ++count[nums[i]];
         
         for (int i = 0; i < nums.length / 3; ++i){
-            int[] temp = new int[3];
-            
             while (count[j] < 1) ++j;
             
-            int min = j;
             int l = 0;
             
             while (l < 3){
-                if (j - min > k) return new int[0][0];
-                
                 if (count[j] > 0) {
-                    temp[l++] = j;
+                    ret[i][l++] = j;
                     count[j]--;
                 }
                 else ++j;
             }
-            
-            ret[i] = temp;
+
+            if (ret[i][2] - ret[i][0] > k) return new int[0][0];
         }
         
         return ret;
