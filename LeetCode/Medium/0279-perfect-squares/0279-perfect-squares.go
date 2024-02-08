0e45e0319da4d4b78e154c8e3c778fdde535333b
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2024-02-09 00:03:26 +0900

    Time: 19 ms (82.83%), Space: 5.9 MB (82.32%) - LeetHub

---
+++ LeetCode/Medium/0279-perfect-squares/0279-perfect-squares.go
@@ -0,0 +1,21 @@
+func numSquares(n int) int {
+    dp := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		dp[i] = math.MaxInt32
+	}
+
+	dp[0] = 0
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j*j <= i; j++ {
+			dp[i] = min(dp[i], dp[i-j*j]+1)
+		}
+	}
+
+	return dp[n]
+}
+
+func min(a, b int) int {
+    if a < b { return a }
+    return b
+}
