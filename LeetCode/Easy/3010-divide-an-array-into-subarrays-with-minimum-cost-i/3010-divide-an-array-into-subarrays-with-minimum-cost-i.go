89ec01852635634530c0d11054f1847159e45f6d
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2024-02-17 22:33:31 +0900

    Time: 0 ms (100.00%), Space: 2.8 MB (31.33%) - LeetHub

--- LeetCode/Easy/3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.go
+++ LeetCode/Easy/3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.go
@@ -1,19 +1,14 @@
 func minimumCost(nums []int) int {
     ret := nums[0]
-    left := 2
-    count := make([]int, 51)
+    first := 999
+    second := 999
 
-    for i := 1; i < len(nums); i++ { count[nums[i]]++ }
-
-    i := 1
-
-    for left > 0 {
-        if count[i] > 0 {
-            count[i]--
-            ret += i
-            left--
-        } else { i++ }
+    for i := 1; i < len(nums); i++ {
+        if nums[i] < first {
+            second = first
+            first = nums[i]
+        } else if nums[i] < second { second = nums[i] }
     }
 
-    return ret
+    return ret + first + second
 }
