6b2d80cf7b7d1ac79326671e58ba369c6432645a
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2024-02-20 21:41:54 +0900

    Time: 0 ms (100.00%), Space: 7.1 MB (14.40%) - LeetHub

---
+++ LeetCode/Easy/1518-water-bottles/1518-water-bottles.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int ret = numBottles;
+        int left = 0;
+        
+        while (numBottles > 0){
+            numBottles += left;
+            left = 0;
+            ret += (numBottles + left) / numExchange;
+            left = numBottles % numExchange;
+            numBottles /= numExchange;
+        }
+        
+        return ret;
+    }
+};
