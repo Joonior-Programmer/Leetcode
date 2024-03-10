b8c567f45c7e573d4835b3ad79232047203a7822
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2024-03-10 21:59:59 +0900

    Time: 0 ms (100.00%), Space: 7.4 MB (12.31%) - LeetHub

---
+++ LeetCode/Easy/1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        int ret = num;
+        int curr = 0;
+        int digit = -1;
+
+        while (num > 0) {
+            if (num % 10 == 6) 
+                digit = curr;
+            
+            num /= 10;
+            curr++;
+        }
+
+        if (digit > -1) ret += 3 * pow(10, digit);
+
+        return ret;
+    }
+};
