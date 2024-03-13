d4ba445a6617cff5aa010463909a1009b832c420
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2024-03-13 22:40:48 +0900

    Time: 0 ms (100.00%), Space: 7.3 MB (41.86%) - LeetHub

---
+++ LeetCode/Easy/2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int ret = 1;
+        int curr = 1;
+        int curr_n = (n * (n + 1)) / 2;
+        
+        while (curr <= curr_n){
+            if (curr == curr_n) return ret;
+            curr_n -= ret;
+            ret++;
+            curr += ret;
+        }
+        
+        return -1;
+    }
+};
