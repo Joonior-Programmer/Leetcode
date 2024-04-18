3f34f6fd02ecc460e27bb469a75cbec1763fd5cc
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2024-04-18 23:55:28 +0900

    Time: 0 ms (100.00%), Space: 7.1 MB (96.91%) - LeetHub

---
+++ LeetCode/Easy/1399-count-largest-group/1399-count-largest-group.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countLargestGroup(int n) {
+        int ret = 0;
+        int maxNum = 0;
+        vector<int> count(37);
+        
+        for (int i = 1; i < n + 1; ++i){
+            int curr = i;
+            int temp = 0;
+            
+            while (curr > 0){
+                temp += curr % 10;
+                curr /= 10;
+            }
+            
+            ++count[temp];
+        }
+        
+        for (int i = 1; i < 37; ++i){
+            if (maxNum < count[i]){
+                maxNum = count[i];
+                ret = 1;
+            } else if (maxNum == count[i]) ++ret;
+        }
+        
+        return ret;
+    }
+};
