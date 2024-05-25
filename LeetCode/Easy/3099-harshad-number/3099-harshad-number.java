a227cd2a0a2acc92a32e64a7d91548013200d6ab
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2024-05-25 22:17:48 +0900

    Time: 0 ms (100.00%), Space: 40.1 MB (84.64%) - LeetHub

---
+++ LeetCode/Easy/3099-harshad-number/3099-harshad-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int copyX = x;
+        int ret = 0;
+        
+        while (copyX > 0) {
+            ret += copyX % 10;
+            copyX /= 10;
+        }
+        
+        if (x % ret != 0)  return -1;
+        
+        return ret;
+    }
+}
