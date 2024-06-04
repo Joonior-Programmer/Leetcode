4b96aa458531d1fe4fe94ebc6ed64bc1c99021c4
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2024-06-04 23:14:49 +0900

    Time: 0 ms (100.00%), Space: 2.5 MB (44.16%) - LeetHub

---
+++ LeetCode/Easy/0965-univalued-binary-tree/0965-univalued-binary-tree.go
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isUnivalTree(root *TreeNode) bool {
+    if root == nil { return true }
+        
+    if root.Left != nil && root.Val != root.Left.Val { return false }
+    if root.Right != nil && root.Val != root.Right.Val { return false }
+
+    return isUnivalTree(root.Left) && isUnivalTree(root.Right)
+}
