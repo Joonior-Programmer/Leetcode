e8e0715ca942e178986b73b30dc57018364fd43b
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2024-06-14 22:28:43 +0900

    Time: 9 ms (84.92%), Space: 56.4 MB (96.72%) - LeetHub

---
+++ LeetCode/Medium/0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+        int ret = 0;
+        int[] count = new int[100001];
+
+        for (int v : nums) { ++count[v]; }
+
+        for (int i = 0; i < 100000; i++) {
+            if (count[i] > 1) {
+                int temp = count[i] - 1;
+                ret += temp;
+                count[i+1] += temp;
+            }
+        }
+
+        if (count[100000] > 1) {
+            int temp = count[100000] - 1;
+            ret += temp * (temp + 1) / 2;
+        }
+
+        return ret;
+    }
+}
