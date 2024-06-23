779eb0949a3a801db0ffecbaf446311b6afa371d
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2024-06-23 23:51:19 +0900

    Time: 1 ms (100.00%), Space: 43.7 MB (100.00%) - LeetHub

---
+++ LeetCode/Easy/3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double minimumAverage(int[] nums) {
+        int[] count = new int[51];
+        int l = 1;
+        int r = 50;
+        double ret = 999;
+        
+        for (int v : nums) ++count[v];
+        
+        while (l <= r){
+            while (l <= r && count[l] < 1) ++l;
+            while (l <= r && count[r] < 1) r--;
+            
+            if (l > r) break;
+            
+            ret = Math.min(ret, (double)(l+r) / 2);
+            count[l]--;
+            count[r]--;
+        }
+        
+        return ret;
+    }
+}
