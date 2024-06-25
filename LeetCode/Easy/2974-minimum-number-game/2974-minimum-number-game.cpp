23adeabd0b1ac8e9f34414d4dc66d277c74253e0
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2024-06-25 23:33:34 +0900

    Time: 3 ms (93.46%), Space: 27.6 MB (20.48%) - LeetHub

---
+++ LeetCode/Easy/2974-minimum-number-game/2974-minimum-number-game.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+        vector<int> count(101);
+        int i = 1;
+        int idx = 0;
+        
+        for (int v : nums) ++count[v];
+        
+        while (i < 101){
+            while (i < 101 && count[i] < 1) ++i;
+            if (i > 100) break;
+            nums[idx+1] = i;
+            count[i]--;
+            
+            while (i < 101 && count[i] < 1) ++i;
+            if (i > 100) break;
+            nums[idx] = i;
+            count[i]--;
+            
+            idx += 2;
+        }
+        
+        return nums;
+    }
+};
