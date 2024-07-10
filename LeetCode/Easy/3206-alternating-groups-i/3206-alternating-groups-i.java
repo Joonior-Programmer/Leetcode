a966b258a078228c9793dfd2f355e9c60a0b589f
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2024-07-10 22:32:14 +0900

    Time: 0 ms (100.00%), Space: 43.1 MB (35.77%) - LeetHub

---
+++ LeetCode/Easy/3206-alternating-groups-i/3206-alternating-groups-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors) {
+        int n = colors.length;
+        int ret = 0;
+        
+        for (int i = 1; i < n - 1; ++i) {
+            int curr = colors[i];
+            if (curr != colors[i-1] && curr != colors[i+1]) ++ret;
+        }
+        
+        if (colors[0] != colors[n-1]) {
+            if (colors[0] != colors[1]) ++ret;
+            if (colors[n-1] != colors[n-2]) ++ret;
+        }
+    
+        return ret;
+    }
+}
