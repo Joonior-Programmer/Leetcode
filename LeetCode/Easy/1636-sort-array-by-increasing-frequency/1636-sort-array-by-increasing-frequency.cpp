735ca0c2118edc13d1ce2c7966e0c82b16d32433
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2024-07-23 23:19:13 +0900

    Time: 3 ms (89.91%), Space: 13.8 MB (98.16%) - LeetHub

---
+++ LeetCode/Easy/1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        vector<int> count(201);
+        int total = nums.size();
+        int n = total - 1;
+        
+        for (int v : nums) ++count[v+100];
+        
+        int idx = n;
+        int curr = 100;
+        
+        while (total > 0){
+            int v = -1;
+            for (int i = 0; i < 201; ++i)
+                if (count[i] == curr) {
+                    v = i;
+                    break;
+                }
+            
+            if (v == -1) {
+                curr--;
+                continue;
+            }
+            
+            total -= count[v];
+            
+            for (int i = 0; i < count[v]; ++i) nums[idx--] = v - 100;
+            
+            count[v] = 0;
+        }
+        
+        return nums;
+    }
+};
