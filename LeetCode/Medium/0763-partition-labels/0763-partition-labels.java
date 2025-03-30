10058ad4d6bd8b91ccc18c96dc2328f1e16ce908
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2025-03-30 15:26:36 +0900

    Time: 2 ms (99.38%), Space: 42.1 MB (80.49%) - LeetHub

--- LeetCode/Medium/0763-partition-labels/0763-partition-labels.java
+++ LeetCode/Medium/0763-partition-labels/0763-partition-labels.java
@@ -1,17 +1,17 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        int[][] idxs = new int[26][1];
+        int[] idxs = new int[26];
         char[] sCharArray = s.toCharArray();
 
-        for (int i = 0; i < sCharArray.length; ++i) idxs[sCharArray[i]-97][0] = i;
+        for (int i = 0; i < sCharArray.length; ++i) idxs[sCharArray[i]-97] = i;
 
         int size = 0;
-        int end = idxs[sCharArray[0]-97][0];
+        int end = idxs[sCharArray[0]-97];
         List<Integer> ret = new ArrayList();
 
         for (int i = 0; i < sCharArray.length; ++i){
             ++size;
-            end = Math.max(end, idxs[sCharArray[i]-97][0]);
+            end = Math.max(end, idxs[sCharArray[i]-97]);
             
             if (end == i) {
                 ret.add(size);
