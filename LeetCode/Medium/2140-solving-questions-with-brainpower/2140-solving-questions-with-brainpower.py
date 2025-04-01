a4f81a62f1bed7646dec104ebd15f8dff37b1544
Joon <69222398+Joonior-Programmer@users.noreply.github.com>
2025-04-01 22:31:01 +0900

    Time: 85 ms (66.45%), Space: 60.8 MB (83.17%) - LeetHub

--- LeetCode/Medium/2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ LeetCode/Medium/2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -1,19 +1,10 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
         n = len(questions)
-        dp = [[0,0] for i in range(n+1)] 
+        dp = [0] * (n + 1) 
 
         for i in range(n-1, -1, -1):
             point, bp = questions[i]
+            dp[i] = max(dp[i+1], dp[min(bp + i + 1, n)] + point)
         
-            
-            dp[i][1] = point + max(dp[min(bp+i+1, n)][0], dp[min(bp+i+1, n)][1])
-            dp[i][0] = max(dp[i+1][0], dp[i+1][1])
-        
-        # print(dp)
-
-        return max(dp[0][0], dp[0][1])
-
-        # skip, no skip
-        # [92,65+21],[92,92+65],[74, 74+65],[65, 39],[65,58+65],[65, 5],[65, 49],[65, 65]
-        # [],[],[4,],[2, 2],[0,0]
+        return max(dp)
